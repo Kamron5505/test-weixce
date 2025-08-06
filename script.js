a2782bf83a9edad0175377faa1c57827618bbbc0
Kamron5505 <kama58077@gmail.com>
2025-08-06 20:00:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".showcase-swiper", {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
