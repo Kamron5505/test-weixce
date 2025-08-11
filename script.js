d771c14f0ba52258807c43cbc8dba7044faceb3c
Kamron5505 <kama58077@gmail.com>
2025-08-11 09:43:12 +0500

    Надеюсь сегодня

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const swiper = new Swiper(".showcase-swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+// Когда страница полностью загрузится — скрываем прелоадер, показываем контент
+window.addEventListener("load", () => {
+  document.getElementById("preloader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+});
